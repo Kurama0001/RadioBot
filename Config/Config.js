ac4438418e850f0ad966024ce242709f085b9a2b
Kurama <70342062+Kurama0001@users.noreply.github.com>
2021-12-26 21:40:14 +0100

    Add files via upload

---
+++ Config/Config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  TOKEN: "Bot TOKEN",
+  DBCONNECTION: "URL MongoDB Database ONLINE", //https://www.youtube.com/watch?v=d4-Seyz3zAU
+  LAVALINK: {
+    HOST: "localhost",
+    PORT: 2635,
+    PASSWORD: "Mypassword"
+  }
+}
